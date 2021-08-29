f699914e3914255ec49cf78321ffa84a796c5fd5
ELNA7O <ahmed.hussein00@eng-st.cu.edu.eg>
2021-08-29 14:01:15 +0200

    Alo

---
+++ STD_LIB.h
@@ -0,0 +1,13 @@
+#ifndef STD_LIB_H
+#define STD_LIB_H
+
+ typedef unsigned char 		u8;
+ typedef signed char 		s8;
+ typedef unsigned short int u16;
+ typedef unsigned short int s16;
+ typedef unsigned long int  u32;
+ typedef unsigned long int  s32;
+ typedef float			    float32;
+ typedef double			    float64;
+
+#endif
---
+++ main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "STD_LIB.h"
+int add(int x , int y);
+void main(void)
+{
+	u8 x = 10;
+	printf("x = %d" , add(10 , 5));
+	scanf("%d" , &x);
+	
+}
